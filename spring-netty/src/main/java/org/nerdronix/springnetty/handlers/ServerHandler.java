73898f384bb1973a90e56bae12cc527e0ed0e697
aby <abrahammenacherry@gmail.com>
2013-06-15 22:43:43 -0400

    Changes done to move to Netty CR5 version

--- spring-netty/src/main/java/org/nerdronix/springnetty/handlers/ServerHandler.java
+++ spring-netty/src/main/java/org/nerdronix/springnetty/handlers/ServerHandler.java
@@ -1,24 +1,40 @@
 package org.nerdronix.springnetty.handlers;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundMessageHandlerAdapter;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.MessageList;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("serverHandler")
-public class ServerHandler extends ChannelInboundMessageHandlerAdapter<String> {
+public class ServerHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
-	public void messageReceived(ChannelHandlerContext arg0, String msg)
-			throws Exception {
-		System.out.println("Incomng ->" + msg);
+		public void messageReceived(ChannelHandlerContext ctx,
+				MessageList<Object> msgs) throws Exception
+	{
+		Channel channel = ctx.channel();
+		MessageList<String> vals = msgs.cast();
+		for(String str : vals){
+			System.out.print(str);
+			channel.write(str);
+		}
+		vals.releaseAll();
 	}
 	
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		System.out.println("Channel is active");
+		System.out.println("Channel is active\n");
 		super.channelActive(ctx);
+	}
+	
+	@Override
+	public void channelInactive(ChannelHandlerContext ctx) throws Exception
+	{
+		System.out.println("\nChannel is disconnected");
+		super.channelInactive(ctx);
 	}
 }
